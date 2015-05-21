7473992c14578e32daaf257546cea657bd270c5d
vndesign09 <vndesign09@gmail.com>
2015-05-21 23:35:48 +0700

    2

---
+++ public/ntd/includes/widget-details-order.php
@@ -0,0 +1,61 @@
+<div class="widget row management-order">
+	<h3>Quản lý đơn hàng</h3>
+	<ul class="menu-images-icons">
+		<li><a href="#"><img src="assets/images/icons-ntd/users.png">Đơn hàng đang sử dụng</a></li>
+		<li><a href="#"><img src="assets/images/chi-tiet.png">Đơn hàng hết hạn/Đã sử dụng</a></li>
+		<li><a href="#"><img src="assets/images/cart.png">Mua dịch vụ</a></li>
+		<li>
+			<div class="col-sm-9 push-top">
+				<h4><i class="fa fa-home"></i> Công ty TNHH Minh Phúc</h4>	
+			</div>
+			<div class="col-sm-3 push-top pull-right">
+			 	<i class="fa fa-edit"></i><a class="text-blue italic decoration">Sửa</a>
+			</div>
+			<ul class="arrow-square-orange">
+				<li>
+					<div class="push-padding-10-lr">
+						Việc làm đang đăng: <span class="text-orange">0</span> vị trí
+					</div>
+				</li>
+				<li>
+					<div class="push-padding-10-lr">
+						Việc làm chưa sử dụng <span class="text-orange">0</span> vị trí.
+					</div>
+				</li>
+				<li>
+					<div class="push-padding-10-lr">
+						Dịch vụ tìm hồ sơ <span class="text-orange">0</span> CV.
+					</div>
+				</li>
+			</ul>
+			<div class="push-padding-10-lr clearfix">
+				<span class="text-orage">0</span> CV, <span class="text-orage">19</span> ngày
+				(Từ 24/04/2015 - 23/05/2015)
+			</div>
+		</li>
+	</ul>	
+</div>
+<div class="support clearfix push-top-30">
+	<div class="heading-image"></div>	
+	<div class="support-info">
+		<h2 class="gotham text-align-center text-blue">Chúng tôi sẵn sàng<br>hỗ trợ quý khách</h2>
+		<img src="assets/images/ruibu.jpg" class="push-top">
+		<ul>
+			<li>
+				<h3><strong><i class="fa fa-play text-blue"></i> Bộ phận kinh doanh</strong></h3>
+				<ul>
+					<li>Tại TP Hồ Chí Minh</li>
+					<li class="text-blue">Tel: (84 8) 3 925 8456</li>
+					<li>Tại Hà Nội</li>
+					<li class="text-blue">Tel: (04 4) 3 974 3033</li>
+					<li class="text-blue">Email: sales@vnjobs.vn</li>
+				</ul>
+			</li>
+			<li>
+				<h3><strong><i class="fa fa-play text-blue"></i>  Bộ phận Chăm sóc khách hàng</strong></h3>
+				<p>Nếu quý khách có thắc mắc nào, vui lòng nhấp vào đây hoặc liên lạc qua đường dây nóng <span class="h3"><strong>1800 7100</strong></span></p>
+			</li>
+		</ul>
+
+	</div>
+</div>
---
+++ public/ntd/qltd-chitietdonhang.php
@@ -0,0 +1,79 @@
+<?php include('includes/header.php') ?>
+	<?php include('includes/menu-tab.php') ?>
+	<section class="boxed-content-wrapper clearfix">
+		<div class="container">
+			<aside id="sidebar" class="col-sm-3">
+				<?php include('includes/widget-details-order.php') ?>
+			</aside>
+			<section id="content" class="pull-right right">
+					<div class="heading-image">
+						<h2 class="text-blue"><img src="assets/images/chi-tiet-don-hang.png">Chi tiết đơn hàng</h2>
+					</div>
+					<div class="filter">
+						<span>Số đơn hàng: <span class="text-orange">15031229</span> | Ngày kích hoạt: <span class="text-orange">25-03-2015</span></span>
+					</div>
+					<table class="table table-blue-bordered table-bordered">
+						<thead>
+							<tr>
+								<th class="col-sm-4">Gói dịch vụ</th>
+								<th class="col-sm-1">Số lượng</th>
+								<th class="col-sm-1">Còn lại</th>
+								<th class="col-sm-2">Ngày bắt đầu kích hoạt</th>
+								<th class="col-sm-2">Ngày kết thúc kích hoạt</th>
+								<th class="col-sm-2">Tình trạng</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Tìm hồ sơ ứng viên RD 50CV
+								<p><i class="fa fa-play text-orange"></i>&nbsp; Tìm kiếm hồ sơ ứng viên</p>
+								</td>
+								<td>600</td>
+								<td>537</td>
+								<td>25-03-2015</td>
+								<td>19-03-2016</td>
+								<td><a class="text-blue">Xem báo cáo</td>
+							</tr>
+							<tr>
+								<td>Tìm hồ sơ ứng viên RD 50CV
+								<p><i class="fa fa-play text-orange"></i>&nbsp; Tìm kiếm hồ sơ ứng viên</p>
+								</td>
+								<td>600</td>
+								<td>537</td>
+								<td>25-03-2015</td>
+								<td>19-03-2016</td>
+								<td><a class="text-blue">Xem báo cáo</td>
+							</tr>
+						</tbody>
+					</table>
+					<div class="detals-package push-top">
+						<div class="heading-border-blue">
+							<h3>Chi tiết sử dụng gói dịch vụ đăng tuyển</h3>
+						</div>
+						<table class="table table-bordered table-little-blue">
+							<thead>
+								<tr>
+									<th>gói dịch vụ</th>
+									<th>chức danh</th>
+									<th>từ ngày</th>
+									<th>đến ngày<br><small class="legend text-orange">[Khuyến mãi]</small></th>
+									<th>Tình trạng</th>
+									<th>Người đăng</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+			</section>
+		</div>
+	</section>
+<?php include('includes/footer.php') ?>
