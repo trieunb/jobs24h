164ae393b9464e5c51bb65ecd388552982be0e67
unknown <vndesign09@gmail.com>
2015-05-21 17:51:10 +0700

    2

---
+++ public/ntd/qltd-chitietbaocao.php
@@ -0,0 +1,171 @@
+<?php include('includes/header.php') ?>
+	<?php include('includes/menu-tab.php') ?>
+	<section class="boxed-content-wrapper clearfix">
+		<div class="container">
+			<aside id="sidebar" class="col-sm-3">
+				<?php include('includes/widget-details-info.php') ?>
+				<?php include('includes/widget-management-order.php') ?>
+			</aside>
+			<section id="content" class="pull-right right">
+					<div class="heading-image">
+						<h2 class="text-blue"><img src="assets/images/list.png">Danh sách gói hồ sơ đã sử dụng</h2>
+					</div>
+					<div class="filter">
+						<span>Hiển thi <span class="text-orange">1</span> - <span class="text-orange">6</span> của <span class="text-orange">6</span> đơn hàng.</span>
+					</div>
+					<table class="table table-blue-bordered table-bordered">
+						<thead>
+							<tr>
+								<th class="col-sm-4">Gói dịch vụ</th>
+								<th class="col-sm-1">Số lượng</th>
+								<th class="col-sm-1">Còn lại</th>
+								<th class="col-sm-2">Ngày bắt đầu kích hoạt</th>
+								<th class="col-sm-2">Ngày kết thúc kích hoạt</th>
+								<th class="col-sm-2">Tình trạng</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Tìm hồ sơ ứng viên RD 50CV</td>
+								<td>600</td>
+								<td>537</td>
+								<td>25-03-2015</td>
+								<td>19-03-2016</td>
+								<td><a class="text-blue">Xem báo cáo</td>
+							</tr>
+							<tr>
+								<td>Tìm hồ sơ ứng viên RD 50CV</td>
+								<td>600</td>
+								<td>537</td>
+								<td>25-03-2015</td>
+								<td>19-03-2016</td>
+								<td><a class="text-blue">Xem báo cáo</td>
+							</tr>
+							<tr>
+								<td>Tìm hồ sơ ứng viên RD 50CV</td>
+								<td>600</td>
+								<td>537</td>
+								<td>25-03-2015</td>
+								<td>19-03-2016</td>
+								<td><a class="text-blue">Xem báo cáo</td>
+							</tr>
+							<tr>
+								<td>Tìm hồ sơ ứng viên RD 50CV</td>
+								<td>600</td>
+								<td>537</td>
+								<td>25-03-2015</td>
+								<td>19-03-2016</td>
+								<td><a class="text-blue">Xem báo cáo</td>
+							</tr>
+							<tr>
+								<td>Tìm hồ sơ ứng viên RD 50CV</td>
+								<td>600</td>
+								<td>537</td>
+								<td>25-03-2015</td>
+								<td>19-03-2016</td>
+								<td><a class="text-blue">Xem báo cáo</td>
+							</tr>
+							<tr>
+								<td>Tìm hồ sơ ứng viên RD 50CV</td>
+								<td>600</td>
+								<td>537</td>
+								<td>25-03-2015</td>
+								<td>19-03-2016</td>
+								<td><a class="text-blue">Xem báo cáo</td>
+							</tr>
+							<tr>
+								<td>Tìm hồ sơ ứng viên RD 50CV</td>
+								<td>600</td>
+								<td>537</td>
+								<td>25-03-2015</td>
+								<td>19-03-2016</td>
+								<td><a class="text-blue">Xem báo cáo</td>
+							</tr>
+						</tbody>
+					</table>
+					<div class="report-resums">
+						<div class="heading-image">
+							<h2 class="text-blue"><img src="assets/images/report-resume.png">Báo cáo xem hồ sơ</h2>
+						</div>
+						<div class="filter">
+							<span>Số lương hồ sơ còn lại: <span class="text-orange">112</span>
+							<span class="pull-right">
+								Ngày bắt đầu sử dụng: <span class="text-orange">25-03-2015</span> - Ngày hết hạn sử dụng: <span class="text-orange">19-03-2016</span>
+							</span>
+						</div>
+						<table class="table table-blue-bordered table-bordered">
+							<thead>
+								<tr>
+									<th class="col-sm-6 text-align-center">Ngày Sử Dụng</th>
+									<th class="col-sm-6 text-align-center">Số lượng hồ sơ Sử Dụng</th>
+								</tr>
+							</thead>
+							<tbody class="text-align-center">
+								<tr>
+									<td>23-04-2015</td>
+									<td>11</td>
+								</tr>
+								<tr>
+									<td>23-04-2015</td>
+									<td>11</td>
+								</tr>
+								<tr>
+									<td>23-04-2015</td>
+									<td>11</td>
+								</tr>
+								<tr>
+									<td>23-04-2015</td>
+									<td>11</td>
+								</tr>
+								<tr>
+									<td>23-04-2015</td>
+									<td>11</td>
+								</tr>
+								<tr>
+									<td>23-04-2015</td>
+									<td>11</td>
+								</tr>
+								<tr>
+									<td>23-04-2015</td>
+									<td>11</td>
+								</tr>
+								<tr>
+									<td>23-04-2015</td>
+									<td>11</td>
+								</tr>
+								<tr>
+									<td>23-04-2015</td>
+									<td>11</td>
+								</tr>
+								<tr>
+									<td>23-04-2015</td>
+									<td>11</td>
+								</tr>
+								<tr>
+									<td>23-04-2015</td>
+									<td>11</td>
+								</tr>
+								<tr>
+									<td>23-04-2015</td>
+									<td>11</td>
+								</tr>
+								<tr>
+									<td>23-04-2015</td>
+									<td>11</td>
+								</tr>
+								<tr>
+									<td>23-04-2015</td>
+									<td>11</td>
+								</tr>
+								<tr>
+									<td>23-04-2015</td>
+									<td>11</td>
+								</tr>
+							</tbody>
+						</table>
+							<a class="text-blue decoration italic"><img src="assets/images/excel.png">Chuyển danh sách sang file excel</a>
+					</div>
+			</section>
+		</div>
+	</section>
+<?php include('includes/footer.php') ?>
---
+++ public/ntd/qltd-thongbaohoso.php
@@ -0,0 +1,14 @@
+<?php include('includes/header.php') ?>
+	<?php include('includes/menu-tab.php') ?>
+	<section class="boxed-content-wrapper clearfix">
+		<div class="boxed">
+			<div class="heading-image">
+				<h2 class="text-orange">Thông báo Hồ sơ & kết quả tìm kiếm đã lưu</h2>
+			</div>
+			<h3>Bạn hiện có: <span class="text-orange">0 Thông báo hồ sơ/Kết quả tìm kiếm đã lưu</span></h3>
+			<h3>Đăng ký Thông Báo Hồ Sơ để nhận hồ sơ ứng viên phù hợp với yêu cầu của Quý khách qua email.<br>Giúp quý khách tiệt kiệm thời gian và hoàn toàn miễn phí.</h3>
+			<button type="button" class="btn btn-lg bg-orange push-top">Tạo mới</button>
+			<p><a href="#" class="text-blue decoration push-top"><i class="fa fa-backward"></i> Trở lại</a></p>
+		</div>
+	</section>
+<?php include('includes/footer.php') ?>
