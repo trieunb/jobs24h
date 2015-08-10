5f531ad204484e621b1d22b47de6c0120704f119
phongdatgl <phongdatgl@gmail.com>
2015-08-10 15:34:16 +0700

    z

--- app/controllers/NTD/HomeController.php
+++ app/controllers/NTD/HomeController.php
@@ -1,11 +1,11 @@
 <?php 
 namespace NTD;
-use View;
+use View,Redirect;
 class HomeController extends \Controller {
 	
 	public function home()
 	{
-		return "Dash board";
+		return Redirect::route('employers.launching');
 	}
 	public function launching()
 	{
